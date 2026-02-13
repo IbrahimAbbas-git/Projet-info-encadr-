1b2a37795030a552634d240179f3ba0cc85d99aa
Nahel Gana <nahel.gana@etu.univ-lyon1.fr>
2026-02-13 14:21:25 +0100

    Ajout cases en rouges

--- MF.java
+++ MF.java
@@ -1,38 +1,44 @@
 import java.util.Observable;
 import java.util.Observer;
-import java.awt.GridLayout;
-import javax.swing.JFrame;
-import javax.swing.JPanel;
+import java.awt.*;
+import javax.swing.*;
 
 class MF extends JFrame implements Observer {
-    JPanel[][] tab = new JPanel[10][10] ;
+    JButton[][] tab = new JButton[10][10] ;
     public MF(Jeu j) {
-        build(j);
+        build(j) ;
 
-        setTitle("Démineur");
-        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        pack(); // ajuste à la taille des composants
-        setLocationRelativeTo(null); // centre la fenêtre
+        setTitle("Démineur") ;
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE) ;
+        pack() ; // ajuste à la taille des composants
+        setLocationRelativeTo(null) ; // centre la fenêtre
     }
 
     public void update(Observable o, Object jeu) {
-        Jeu j = (Jeu) jeu ;
-        tab[j.i][j.j].setBackground(java.awt.Color.RED);
+        Jeu j = (Jeu) jeu;
+        tab[j.i][j.j].setBackground(Color.RED);
     }
+
     public void build(Jeu jeu) {
-        JPanel pp = new JPanel(new GridLayout(10, 10)) ;
-        for (int i = 0 ; i < 10 ; i++) {
-            for (int j = 0 ; j < 10 ; j++) {
-                //if(i==jeu.i && j==jeu.j) {
-                //    tab[i][j] = new JPanel() ;
-                //    tab[i][j].setBackground(java.awt.Color.RED);
-               // } else {
-                tab[i][j] = new JPanel() ;
-                var text = new javax.swing.JLabel(i + "," + j) ;
-                tab[i][j].add(text);
-                pp.add(tab[i][j]) ;//}
+        JPanel pp = new JPanel(new GridLayout(10, 10));
+
+        for (int i = 0; i < 10; i++) {
+            for (int j = 0; j < 10; j++) {
+
+                tab[i][j] = new JButton();
+                tab[i][j].setPreferredSize(new Dimension(50, 50));
+
+                int x = i;
+                int y = j;
+
+                tab[i][j].addActionListener(e -> {
+                    jeu.set(x, y);
+                });
+
+                pp.add(tab[i][j]);
             }
         }
+
         this.add(pp);
     }
     
--- main.java
+++ main.java
@@ -6,7 +6,7 @@ public class main {
     public static void main(String[] args){
     Jeu j = new Jeu() ;
     JFrame mf = new MF(j) ;
-    j.addObserver((Observer)mf) ;
+    j.addObserver((Observer) mf) ;
     SwingUtilities.invokeLater(
         new Runnable() {
             public void run() {
